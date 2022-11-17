31a898f87e999494e982ebfb2392d4c956a63b9b
aolsx <aolsx@hotmail.com>
2022-11-18 03:59:48 +0800

    up

--- nui/index.js
+++ nui/index.js
@@ -7,8 +7,8 @@ import './assets/style/index.css';
 
 // 实例化之后 安装
 export default function(app){
-    // 挂载全局 handler
-    app.config.globalProperties.$Nui = handlers;
-    // 遍历注册全局组件
-    components.map(component=>app.component(component.name,component));
-};
+  // 挂载全局 handler
+  app.config.globalProperties.$Nui = handlers;
+  // 遍历注册全局组件
+  components.map(component=>app.component(component.name,component));
+}
